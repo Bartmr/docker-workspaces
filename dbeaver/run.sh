28fd9960b7938c24d312514cf81475977ae70760
Bartolomeu <pbartmr@gmail.com>
2022-06-15 16:37:12 +0100

    Commit

--- dbeaver/run.sh
+++ dbeaver/run.sh
@@ -33,8 +33,6 @@ then
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     -v $(pwd)/data:/usr/src/app \
-    --device /dev/snd \
-    --device /dev/dri \
     --name dbeaver \
     dbeaver
 else
