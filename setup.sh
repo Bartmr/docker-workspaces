3bcaa631b7ad9ec538cec31af566e129d5d7da18
Bartolomeu <pbartmr@gmail.com>
2022-06-23 10:39:48 +0100

    Commit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 
-if [ DOCKER_WORKSPACES_SET_UP != "true" ]
+if [ "$DOCKER_WORKSPACES_SET_UP" != "true" ]
 then
   echo  '
 # From docker-workspaces repository
 alias prune-volumes="docker volume list --filter dangling=true --format \"{{.Name}}\" | grep -v \"chrome-data\" | xargs docker volume rm"
 
-export DOCKER_WORKSPACES_SET_UP=true' >> ~/.bashrc 
+export DOCKER_WORKSPACES_SET_UP="true"' >> ~/.bashrc
+
+  export DOCKER_WORKSPACES_SET_UP="true"
 fi
