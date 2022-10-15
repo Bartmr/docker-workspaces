039f6f677f9e5a23db6bc53212393c61070637ca
Bartolomeu <pbartmr@gmail.com>
2022-10-15 13:52:49 +0100

    Improvements to chrome docker image

--- shared/rebuild.sh
+++ shared/rebuild.sh
@@ -5,10 +5,12 @@ name=$1
 docker container rm "bartmr-docker-workspaces-$name"
 
 set -e 
+
+docker_workspaces_dir="$(dirname "$(dirname "$(realpath $0)")")"
   
 docker build \
   --build-arg USER_UID=$(id -u) \
   --build-arg USER_GID=$(id -g) \
   --build-arg TZ=$(cat /etc/timezone) \
   --build-arg CACHEBUST=$(date +%s) \
-  --tag "bartmr-docker-workspaces/$name" "../$name"
+  --tag "bartmr-docker-workspaces/$name" "$docker_workspaces_dir/$name"
