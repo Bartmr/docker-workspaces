2ef2aae4b142dfb702d1cd2c857b362e09a50007
Bartolomeu <pbartmr@gmail.com>
2022-10-17 22:06:13 +0100

    Commit

--- dbeaver/run.sh
+++ dbeaver/run.sh
@@ -1,44 +1,7 @@
 #!/usr/bin/env bash
-
-# https://blog.jessfraz.com/post/docker-containers-on-the-desktop/
-
-docker inspect "dbeaver:latest" > /dev/null 2>&1
-LAST_RESULT=$?
-
 set -e
 
-if [ $LAST_RESULT -ne 0 ]
-then
-  docker build \
-    --build-arg USER_UID=$(id -u) \
-    --build-arg USER_GID=$(id -g) \
-    --tag dbeaver .
-fi
+docker_workspaces_dir="$(dirname "$(dirname "$(realpath $0)")")"
 
-mkdir -p ./data
-chmod go+rw ./data
-
-xhost +"local:docker@"
-
-set +e
-
-docker container inspect dbeaver  > /dev/null 2>&1
-LAST_RESULT=$?
-
-set -e
-
-if [ $LAST_RESULT -ne 0 ]
-then
-  # Might need to use 
-  # -v /dev/shm:/dev/shm \
-
-  docker run -d \
-    --net host \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e DISPLAY=unix$DISPLAY \
-    -v $(pwd)/data:/home/dbeaver/data \
-    --name dbeaver \
-    dbeaver
-else
-  docker start dbeaver
-fi
+$docker_workspaces_dir/shared/run.sh dbeaver \
+  --net host
