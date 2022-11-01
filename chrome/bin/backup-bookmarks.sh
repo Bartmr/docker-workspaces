2dca389b802590ebfbf1f0b46e53258fa3eed97d
Bartolomeu <pbartmr@gmail.com>
2022-11-01 21:05:17 +0000

    Commit

---
+++ chrome/bin/backup-bookmarks.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+# Use [[ $# -gt 0 ]] to test existence of positional parameters.
+
+
+#
+
+mkdir -p /home/chrome/docker-data/chrome-backup/Default
+cp /home/chrome/chrome-data/Default/Bookmarks /home/chrome/docker-data/chrome-backup/Default/
+
+find /home/chrome/chrome-data -maxdepth 1 -type d -name "Profile *" -print0 | while read -d '' -r profile_dir
+do
+  profile_name=$(basename "${profile_dir}")
+
+  mkdir -p "/home/chrome/docker-data/chrome-backup/${profile_name}"
+
+  cp "/home/chrome/chrome-data/${profile_name}/Bookmarks" "/home/chrome/docker-data/chrome-backup/${profile_name}/"
+done
