0f44e8e715b5d1a988df920de1c0f5b2dbafa27c
Bartolomeu <pbartmr@gmail.com>
2022-12-24 17:19:59 +0000

    Commit

---
+++ chrome/bin/get-signed-in-accounts.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+
+jq ".account_info[0].email" "/home/chrome/chrome-data/Default/Preferences"
+
+find /home/chrome/chrome-data -maxdepth 1 -type d -name "Profile *" -print0 | while read -d '' -r profile_dir
+do
+  jq ".account_info[0].email" "$profile_dir/Preferences"
+done
